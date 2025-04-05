ec647e9d7d8e62157fa3dbe578013ae5b2d838ab
seema patil <seemampatil22@gmail.com>
2025-04-05 10:12:47 +0530

    Add files via upload
      revers String

---
+++ Revers.java
@@ -0,0 +1,12 @@
+public class Revers {
+  
+    public static void main(String[] args) {
+        String s="java";
+       String res="";
+        for(int i=s.length()-1;i>=0;i--){
+           res+=s.charAt(i);
+
+        }
+        System.out.println(res);
+    }
+}
