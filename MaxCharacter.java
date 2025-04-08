8f65e8866dfa76692c9c7e29f09202f53cbf3a18
seema patil <seemampatil22@gmail.com>
2025-04-08 10:04:56 +0530

    Add files via upload
    Max-Character program

---
+++ MaxCharacter.java
@@ -0,0 +1,31 @@
+public class MaxCharacter {
+    public static void main(String[] args) {
+        String s = "javvva";
+        char[] ch = s.toCharArray();
+        int maxCount = 0;
+        char maxChar = ch[0];
+        int secMaxCount=0;
+        int secMaxChar=ch[0];
+
+        for (int i = 0; i < ch.length; i++) {
+            
+            int count = 1;
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j]) {
+                    count++;
+                }
+            }
+
+            if (count>maxCount) {
+                maxCount = count;
+                maxChar = ch[i];
+                if(count>secMaxCount&& ch[i]!=maxChar){
+                    secMaxCount=count;
+                    secMaxChar=ch[i];
+                }
+            }
+        }
+
+        System.out.println("Most frequent character: " + secMaxChar+ " = " + secMaxChar);
+    }
+}
