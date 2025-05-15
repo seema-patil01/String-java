4311e9705c42b0105a8387486c68231ccfc02e2f
seema patil <seemampatil22@gmail.com>
2025-05-15 10:03:56 +0530

    Add files via upload
    space program

---
+++ space.java
@@ -0,0 +1,13 @@
+public class space {
+    public static void main(String[] args) {
+        String input="java       is      easy";
+        
+        String output = input.trim().replaceAll("\\s+", " ");
+
+        System.out.println(output) ;
+
+       
+    }
+}
+
+
