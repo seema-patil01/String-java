7c0534f5dc35097fe37d0e51ea1849ed9a70cb5b
seema patil <seemampatil22@gmail.com>
2025-05-20 12:45:47 +0530

    Add files via upload
    index printing

--- Indexprint.java
+++ Indexprint.java
@@ -1,28 +1,26 @@
 public class Indexprint {
-  
     public static void main(String[] args) {
-        String s = "banana";
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            boolean flag = false;
-            for (int j = 0; j < i; j++) {
-                if (s.charAt(j) == current) {
-                    flag= true;
-                    break;
+        String str = "banana";
+        String done = ""; 
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (done.indexOf(ch) != -1) continue;
+
+            System.out.print(ch + "=");
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == ch) {
+                    System.out.print(j);
+
+                 
+                    int next = str.indexOf(ch, j + 1);
+                    if (next != -1) System.out.print(",");
                 }
-            } 
-            if (flag) {
-                continue;
             }
-              System.out.print(current + " = ");
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == current) {
-                    System.out.print(j + " ");
-                }
-            }
+
             System.out.println();
+            done += ch;
         }
     }
 }
-
-
