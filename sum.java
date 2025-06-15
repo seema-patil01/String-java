87e71f3ff2de43e09ee4fc5669466a4658c073ba
seema patil <seemampatil22@gmail.com>
2025-06-15 13:46:59 +0530

    Add files via upload
    Sum of two string

---
+++ sum.java
@@ -0,0 +1,15 @@
+public class sum {
+    
+    
+    public static void main(String[] args) {
+        String s1 = "25";
+        String s2 = "23";
+        int num1 = Integer.parseInt(s1);
+        int num2 = Integer.parseInt(s2);
+        int sum = num1 + num2;
+        String result = Integer.toString(sum);
+        System.out.println("Output: \"" + result + "\"");
+    }
+}
+
+
